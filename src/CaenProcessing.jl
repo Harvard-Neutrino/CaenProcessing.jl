f77f07693fa61ecab63e224cbd4dbfe9e16a4174
kcarloni <kcarloni@g.harvard.edu>
2025-06-26 15:25:43 -0400

    remove all extraneous functions

--- src/CaenProcessing.jl
+++ src/CaenProcessing.jl
@@ -19,17 +19,8 @@ const mV = u"mV"
 const C = u"C"
 
 include("read_and_process.jl")
-include("coincidence.jl")
-
-include("langau.jl")
-include("distributions.jl")
-include("fitting.jl")
 
 export ns, mV, C
 export read_waveforms, process_data
-export get_coincidence_mask
-
-export fit_muon_distribution, find_muon_Vmin
-export Gaussian, Langau
 
 end
