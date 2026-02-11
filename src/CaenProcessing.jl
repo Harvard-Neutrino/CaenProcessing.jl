6f83c276bfaf350959f2028f960da5561c2b980b
kcarloni <kcarloni@g.harvard.edu>
2026-02-11 09:25:29 -0500

    fix (?)

--- src/CaenProcessing.jl
+++ src/CaenProcessing.jl
@@ -9,10 +9,11 @@ using StatsBase
 const ns = u"ns"
 const mV = u"mV"
 const C = u"C"
+const pC = u"pC"
 
 include("read_and_process.jl")
 
-export ns, mV, C
+export ns, mV, C, pC
 export read_waveforms, process_data
 
 export volts_per_ADC, time_per_sample
